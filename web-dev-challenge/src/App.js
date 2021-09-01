c66b2059610a83dd9223c84850f48bbfa85a0974
Brian Kim <briaansjkim@gmail.com>
2021-09-01 12:25:13 -0400

    added state in order to store and render user input

--- web-dev-challenge/src/App.js
+++ web-dev-challenge/src/App.js
@@ -2,13 +2,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      // toDo will be an empty string first to take in the user input
+      toDo: '',
+      toDoList: []
+    };
+  };
+
   render() {
     return (
       <div>
         <h1>Hi There</h1>
       </div>
-    )
-  }
+    );
+  };
 };
 
 export default App;
