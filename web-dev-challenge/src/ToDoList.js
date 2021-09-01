fcb408e408eb05bd62ef28160dcdbad37435cee5
Brian Kim <briaansjkim@gmail.com>
2021-09-01 13:24:50 -0400

    added paragraph tag and text for zero state display

--- web-dev-challenge/src/ToDoList.js
+++ web-dev-challenge/src/ToDoList.js
@@ -1,4 +1,9 @@
 import React from 'react';
 
 export default function ToDoList() {
+    return (
+        <div>
+            <p>You have no items in your todo list. Create an item to begin tracking your list.</p>
+        </div>
+    )
 };
