5775b4df5557a854cf6e999c5e9cdfe7e2b2c37a
Brian Kim <briaansjkim@gmail.com>
2021-09-01 13:03:29 -0400

    added todo heading

--- web-dev-challenge/src/App.js
+++ web-dev-challenge/src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1>TODO</h1>
         <textarea 
           onChange={this.handleChange}
           onKeyPress={this.handleSubmit}
