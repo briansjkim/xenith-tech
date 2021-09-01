23280611bc63ededcdfd34b63e7f95b34f9c3731
Brian Kim <briaansjkim@gmail.com>
2021-09-01 12:22:41 -0400

    changed to class component in order to account for state and events

--- web-dev-challenge/src/App.js
+++ web-dev-challenge/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+// import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi There</h1>
+      </div>
+    )
+  }
+};
 
 export default App;
