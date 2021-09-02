9874116a4200894061e8a51a4ec89203fa5d43c1
Brian Kim <briaansjkim@gmail.com>
2021-09-02 13:12:40 -0400

    initial todo list

--- web-dev-challenge/src/TodoList/TodoList.js
+++ web-dev-challenge/src/TodoList/TodoList.js
@@ -3,9 +3,15 @@ import PropTypes from 'prop-types';
 import './TodoList.css';
 
 export default function TodoList({ todos }) {
+
     return (
         <div className="Todolist">
             {todos.length === 0 && <p>You have no items in your todo list. Create an item to begin tracking your list.</p>}
+            <ul>
+                {todos.map( todo =>
+                    <li>{todo}</li>
+                )}
+            </ul>
         </div>
     )
 }
