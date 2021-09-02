607b535c300290ba96a5b57c9be59c0694e2df83
Brian Kim <briaansjkim@gmail.com>
2021-09-02 13:19:01 -0400

    edited generateKey function to be more unique

--- web-dev-challenge/src/TodoList/TodoList.js
+++ web-dev-challenge/src/TodoList/TodoList.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 import './TodoList.css';
 
 export default function TodoList({ todos }) {
-    const generateKey = () => {
-        return `${new Date().getTime() }`;
+    const generateKey = (item) => {
+        return `${item}_${new Date().getTime() }`;
     };
 
     return (
         <div className="Todolist">
             {todos.length === 0 && <p>You have no items in your todo list. Create an item to begin tracking your list.</p>}
-            <ul>
+            <ul className="todos">
                 {todos.map( todo =>
-                    <li key={generateKey()}>{todo}</li>
+                    <li key={generateKey(todo)}>{todo}</li>
                 )}
             </ul>
         </div>
