08247ba907fce3298581623c6101cfc3d4ff3a4a
Brian Kim <briaansjkim@gmail.com>
2021-09-02 13:13:09 -0400

    used function in order to create and set key prop

--- web-dev-challenge/src/TodoList/TodoList.js
+++ web-dev-challenge/src/TodoList/TodoList.js
@@ -6,13 +6,13 @@ export default function TodoList({ todos }) {
     const generateKey = () => {
         return `${new Date().getTime() }`;
     };
-    
+
     return (
         <div className="Todolist">
             {todos.length === 0 && <p>You have no items in your todo list. Create an item to begin tracking your list.</p>}
             <ul>
                 {todos.map( todo =>
-                    <li>{todo}</li>
+                    <li key={generateKey()}>{todo}</li>
                 )}
             </ul>
         </div>
