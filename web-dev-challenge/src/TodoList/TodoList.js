00da28556d9e29614d87428b6c0e50ce82c63a69
Brian Kim <briaansjkim@gmail.com>
2021-09-02 13:12:51 -0400

    created function for key prop

--- web-dev-challenge/src/TodoList/TodoList.js
+++ web-dev-challenge/src/TodoList/TodoList.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import './TodoList.css';
 
 export default function TodoList({ todos }) {
-
+    const generateKey = () => {
+        return `${new Date().getTime() }`;
+    };
+    
     return (
         <div className="Todolist">
             {todos.length === 0 && <p>You have no items in your todo list. Create an item to begin tracking your list.</p>}
