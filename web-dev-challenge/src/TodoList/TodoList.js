424256b8e130f284b1dfd145ee7fca4f7e74d0c1
Brian Kim <briaansjkim@gmail.com>
2021-09-02 22:40:30 -0400

    edited path for ListItem

--- web-dev-challenge/src/TodoList/TodoList.js
+++ web-dev-challenge/src/TodoList/TodoList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './TodoList.css';
-import ListItem from '../ListItem';
+import ListItem from '../ListItem/ListItem';
 // import Button from '../Button/Button';
 
 export default function TodoList({ todos }) {
