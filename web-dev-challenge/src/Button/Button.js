0057024f4b03c511bc32708198e1d6f124a108a2
Brian Kim <briaansjkim@gmail.com>
2021-09-02 22:40:11 -0400

    commented out Buttom component for now

--- web-dev-challenge/src/Button/Button.js
+++ web-dev-challenge/src/Button/Button.js
@@ -6,39 +6,37 @@ import './Button.css';
 // and App already has its own logic, so wanted to separate Button logic
 
 // maybe use React Hooks later since it only needs one prop in state
-class Button extends Component {
-    constructor(props) {
-        super(props);
+// class Button extends Component {
+//     constructor(props) {
+//         super(props);
 
-        this.state = {
-            defaultBackground: true
-        };
+//         this.state = {
+//             defaultBackground: true
+//         };
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+//         this.handleClick = this.handleClick.bind(this);
+//     }
 
-    handleClick() {
-        this.setState(prevState => {
-          return {
-            defaultBackground: !prevState.defaultBackground
-          };
-        });
-    }
+//     handleClick() {
+//         this.setState(prevState => {
+//           return {
+//             defaultBackground: !prevState.defaultBackground
+//           };
+//         });
+//     }
 
-    render() {
-        return (
-            <button 
-                className={this.state.defaultBackground ? "todo-ellipse" : "todo-complete"} 
-                onClick={this.handleClick}>
-                    {this.props.children}
-                </button>
-        )
-    }
+//     render() {
+//         return (
+//             <button 
+//                 className={this.state.defaultBackground ? "todo-ellipse" : "todo-complete"} 
+//                 onClick={this.handleClick}>
+//                     {this.props.children}
+//                 </button>
+//         )
+//     }
 
-}
+// }
 
-export default Button;
 
-Button.propTypes = {
-    children: PropTypes.string
-};
+
+// export default Button;
