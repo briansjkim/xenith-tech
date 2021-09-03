6a124033e0386e16b96bea7753864fbfc51309e4
Brian Kim <briaansjkim@gmail.com>
2021-09-02 22:32:50 -0400

    testing ListItem as a component with button and todo prop

---
+++ web-dev-challenge/src/ListItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './ListItem.css';
+
+class ListItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            defaultBackground: true
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => {
+          return {
+            defaultBackground: !prevState.defaultBackground
+          };
+        });
+    }
+
+    render() {
+        return (
+            <div className='List-item'>
+                <button
+                    className={!this.state.defaultBackground && 'todo-complete'}
+                    onClick={this.handleClick}
+                ></button>
+                <li>{this.props.todo}</li>
+            </div>
+        )
+    }
+}
+
+ListItem.propTypes = {
+    todo: PropTypes.string
+};
+
+export default ListItem;
