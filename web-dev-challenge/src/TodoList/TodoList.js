4687fa6dab58c285e26cc9015e73cca554ab3ee2
Brian Kim <briaansjkim@gmail.com>
2021-09-02 22:08:52 -0400

    used Button component rather than mapping each button

--- web-dev-challenge/src/TodoList/TodoList.js
+++ web-dev-challenge/src/TodoList/TodoList.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './TodoList.css';
+import Button from '../Button/Button';
 
-export default function TodoList({ todos, handleClick, defaultBackground }) {
+export default function TodoList({ todos }) {
     const generateKey = (item) => {
         return `${item}_${new Date().getTime() }`;
     };
 
     return (
-        // <div className="Todolist">
-        //     <ul className="todos">
-        //         {todos.map( todo =>
-        //             <li key={generateKey(todo)}>{todo}</li>
-        //         )}
-        //     </ul>
-        // </div>
         <div className="Todolist">
             <ul className="todos">
                 {todos.map( todo =>
                     <li key={generateKey(todo)}>
-                        {/* onHover => check mark, onClick => colored check icon and todo item crossed out */}
-                        {/* onClick is being applied on all the elements instead of individually */}
-                        <button className={defaultBackground ? "todo-ellipse" : "todo-complete"} onClick={handleClick}></button>
+                        <Button />
+                        {todo}
+                    </li>
+                )}
+                {/* {todos.map( todo =>
+                    <button className={defaultBackground ? "todo-ellipse" : "todo-complete"} onClick={handleClick}></button>
+                    <li key={generateKey(todo)}>
+                        onHover => check mark, onClick => colored check icon and todo item crossed out
+                        onClick is being applied on all the elements instead of individually
                         {todo}
                     </li>    
-                )}
+                )} */}
             </ul>
             {/* {todos.length !== 0 && <p>{todos.length} items left</p>} */}
         </div>
