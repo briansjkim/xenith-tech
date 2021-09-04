fb4b835ea994c05fe226914f4fa61f5390159a67
Brian Kim <briaansjkim@gmail.com>
2021-09-04 13:40:19 -0400

    added mid section for all, active, completed labels

--- web-dev-challenge/src/Footer/Footer.js
+++ web-dev-challenge/src/Footer/Footer.js
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types';
 import './Footer.css';
 
 // have to access todoList and see how many todoss' defaultBackground's are true
-export default function Footer({ todosLength }) {
+export default function Footer({ todos }) {
     return (
-        <div>
+        <div className="footer">
             <div className="remaining-items">
-                <p>{ todosLength } items left</p>
+                <p>{ todos.length } items left</p>
+            </div>
+            <div className="mid">
+                <p>All</p>
+                <p>Active</p>
+                <p>Completed</p>
             </div>
         </div>
     )
 }
 
 Footer.propTypes = {
-    todosLength: PropTypes.number
+    todos: PropTypes.object
 };
